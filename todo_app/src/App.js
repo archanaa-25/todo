2c5f017f3cc33ee12c4412425085d134d6f6bcf6
archanaa-25 <rarchanaa25@gmail.com>
2025-04-17 18:11:32 +0530

    added timestamp sorting
    deployed using firebase and added timestamp sorting.

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -3,6 +3,7 @@ import {Button, FormControl, Input, InputLabel} from '@mui/material';
 import './App.css';
 import Todo from './Todo';
 import db from './firebase';
+import firebase from 'firebase';
 
 function App() {
   const [todos,setTodos] = useState([])
@@ -11,8 +12,7 @@ function App() {
 //when the app loads, we need to listen to the database and fetch new todos as they get added/removed
 useEffect(() => {
   //this code here...fires when the app.js loads
-  db.collection('todos').onSnapshot(snapshot => {
-    //console.log(snapshot.docs.map(doc => doc.data()))
+  db.collection('todos').orderBy('timestamp','desc').onSnapshot(snapshot => {
     setTodos(snapshot.docs.map(doc => doc.data().todo))
   })
 }, []);
@@ -20,13 +20,19 @@ useEffect(() => {
   const addTodo = (event) => {
     //this will fire off when we click the button
     event.preventDefault(); //will stop refreshing
-    setTodos([...todos,input]);
+
+    db.collection('todos').add({
+      todo : input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    })
+
     setInput('');  //clear up the input after hitting submit
   }
 
   return (
     <div className="App">
       <h1>Hello world</h1>
+      
       <form>
         <FormControl>
           <InputLabel>✔ Write a Todo</InputLabel>
