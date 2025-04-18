030a85c6349897a4a682196631d00d5c5bdb7412
archanaa-25 <rarchanaa25@gmail.com>
2025-04-18 16:45:04 +0530

    Added DEMO video

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -1,61 +1,3 @@
-// import React, { useEffect, useState } from 'react';
-// import {Button, FormControl, Input, InputLabel} from '@mui/material';
-// import './App.css';
-// import Todo from './Todo';
-// import db from './firebase';
-// import firebase from 'firebase';
-
-// function App() {
-//   const [todos,setTodos] = useState([])
-//   const [input,setInput] = useState('');
-
-// //when the app loads, we need to listen to the database and fetch new todos as they get added/removed
-// useEffect(() => {
-//   //this code here...fires when the app.js loads
-//   db.collection('todos').orderBy('timestamp','desc').onSnapshot(snapshot => {
-//     setTodos(snapshot.docs.map(doc => ({id:doc.id, todo:doc.data().todo})))
-//   })
-// }, []);
-
-//   const addTodo = (event) => {
-//     //this will fire off when we click the button
-//     event.preventDefault(); //will stop refreshing
-
-//     db.collection('todos').add({
-//       todo : input,
-//       timestamp: firebase.firestore.FieldValue.serverTimestamp()
-//     })
-
-//     setInput('');  //clear up the input after hitting submit
-//   }
-
-//   return (
-//     <div className="App">
-//       <h1>TODO</h1>
-//       <h3>Reminds Everything</h3>
-      
-//       <form>
-//         <FormControl>
-//           <InputLabel>✔ Add a Task</InputLabel>
-//           <Input value={input} onChange={event => setInput(event.target.value)}/>
-//         </FormControl>
-
-//         <Button disabled={!input} type="submit" onClick={addTodo} variant="contained" color="primary">Add TODO</Button>
-//         {/* <button type="submit" ></button> */}
-//       </form>
-//       <ul>
-//         {todos.map(todo => (
-//           <Todo todo={todo}/>
-//           //<li>{todo}</li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
 import React, { useEffect, useState } from 'react';
 import {Button, FormControl, Input, InputLabel} from '@mui/material';
 import './App.css';
@@ -68,6 +10,8 @@ function App() {
   const [input, setInput] = useState('');
   const [currentDate, setCurrentDate] = useState('');
   const [currentDay, setCurrentDay] = useState('');
+  const [deadline, setDeadline] = useState('');
+
 
   // Format current date when component mounts
   useEffect(() => {
@@ -104,6 +48,7 @@ function App() {
       setTodos(snapshot.docs.map(doc => ({id: doc.id, todo: doc.data().todo})))
     })
   }, []);
+  
 
   const addTodo = (event) => {
     // This will fire off when we click the button
@@ -116,6 +61,7 @@ function App() {
 
     setInput('');  // Clear up the input after hitting submit
   }
+
 
   return (
     <div className="App">
